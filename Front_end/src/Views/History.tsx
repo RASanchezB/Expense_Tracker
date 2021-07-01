4e601ca304ee6853e4b81e890ee95016101cbffe
Ricardo Sanchez <rasanchez@unitec.edu>
2021-06-30 20:15:04 -0600

    Starting new view: History

---
+++ Front_end/src/Views/History.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const History = () =>{
+
+    return(
+        <div>
+            <h2>Taable of Past Expenses </h2>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Amount</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+            <label>:( Bye bye money</label>
+        </div>
+    )
+}
+export default History;
