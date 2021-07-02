ce0e5db0b27340aa6a40f39035f6ddb3ff1331a0
Ricardo Sanchez <rasanchez@unitec.edu>
2021-07-02 15:55:51 -0600

    Mongo Investigation and first conection

--- Back_end/src/index.js
+++ Back_end/src/index.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var app = express();
+require('dotenv').config();
+
+let mongoose = require('mongoose')
+let express = require('express');
+let app = express();
 
 app.get('/',function(req,res){
     res.send('Hello World!');
@@ -8,3 +11,19 @@ app.get('/',function(req,res){
 app.listen(3000,function(){
     console.log('Example app listening on port 3000!')
 });
+
+const MONGO_URI = process.env.MONGO_URI || '';
+
+mongoose.connect(
+  MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log('successfully connected to MongoDB');
+  },
+);
